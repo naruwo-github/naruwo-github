80a8385e6d173bb9b6875009a832500394185df9
Narumi Nogawa <narutyan0711@gmail.com>
2024-01-28 14:54:53 +0900

    Refactor page header and remove redundant code Update page styling and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,14 @@ import { GithubIcon, LinkedinIcon, TwitterIcon } from "@/components/SNSIcons"
 export default function Home() {
   return (
     <main className="flex-1 px-4 md:px-6 shadow-md">
-      <section className="py-12 md:py-24 lg:py-32">
+      <section className="py-5 md:py-10 lg:py-15">
         <div className="container px-4 md:px-6">
-          <div className="space-y-4 text-center">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              {AUTHOR}
-            </h1>
+          <div className="space-y-3 text-center">
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">{AUTHOR}</h2>
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-              Web Developer, Tennis and Snowboard Lover. 🧑‍💻🎾🏂
+              Web(Full-stack) Developer, Tennis and Snowboard Lover. 🧑‍💻🎾🏂
             </p>
-          </div>
-        </div>
-      </section>
-      <section className="py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="space-y-4 text-center">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">About Me</h2>
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-              I&apos;m a full-stack web developer.
-              <br />
               I have experience in a
               variety of technologies and I&apos;m always looking for new challenges to improve User Interface and Developer Experience.
               <br />
@@ -34,10 +23,11 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="py-12 md:py-24 lg:py-32">
+
+      <section className="py-5 md:py-10 lg:py-15">
         <div className="container px-4 md:px-6">
-          <div className="space-y-4 text-center">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Work Experience</h2>
+          <div className="space-y-3 text-center">
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">Work Experience</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               <div className="flex flex-col space-y-2">
                 <h3 className="text-xl font-semibold">Commmune Inc</h3>
@@ -58,10 +48,11 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="py-12 md:py-24 lg:py-32">
+
+      <section className="py-5 md:py-10 lg:py-15">
         <div className="container px-4 md:px-6">
-          <div className="space-y-4 text-center">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Contact</h2>
+          <div className="space-y-3 text-center">
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">Contact</h2>
             <div className="flex flex-col space-y-2">
               <p className="text-gray-500 dark:text-gray-400">{MAIL}</p>
               <div className="flex space-x-4">
