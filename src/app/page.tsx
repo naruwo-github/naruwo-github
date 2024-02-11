3f0fcddcb4bc7a43033573ed88f26fb7d2952b60
Narumi Nogawa <narutyan0711@gmail.com>
2024-02-11 15:25:07 +0900

    🔧 chore(package.json): update lucide-react, @types/node, @types/react, @types/react-dom, postcss, and prettier to latest versions for improved functionality and security ✨ feat(layout.tsx): add main tag with shadow styling for better layout structure ♻️ refactor(page.tsx): modularize sections into separate components for better readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,98 @@
+import React from "react"
 import { AUTHOR, MAIL, URL_LINKEDIN, URL_WANTEDLY } from "../constants/identity"
 import { GithubIcon, LinkedinIcon, TwitterIcon } from "@/components/SNSIcons"
 
+const Author = (urlLinkedin: string, urlWantedly: string) => {
+  return (
+    <React.Fragment>
+      <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+        Web(Full-stack) Developer, Tennis and Snowboard Lover. 🧑‍💻🎾🏂
+      </p>
+      <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+        I have experience in a
+        variety of technologies and I&apos;m always looking for new challenges to improve User Interface and Developer Experience.
+        <br />
+        <br />
+        Please see these links for more information.
+      </p>
+      <a href={urlLinkedin} target="_blank" rel="noopener noreferrer" className="text-blue-700 hover:text-blue-900">LinkedIn</a>
+      , <a href={urlWantedly} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">Wantedly</a>
+    </React.Fragment>
+  )
+}
+
+const WorkExperience = () => {
+  return (
+    <React.Fragment>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="flex flex-col space-y-2">
+          <h3 className="text-xl font-semibold">Commmune Inc</h3>
+          <p className="text-gray-500 dark:text-gray-400">2023 - Present</p>
+          <p className="text-gray-500 dark:text-gray-400">Web Developer/Team Leader</p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <h3 className="text-xl font-semibold">MonotaRO Inc</h3>
+          <p className="text-gray-500 dark:text-gray-400">2021 - 2023</p>
+          <p className="text-gray-500 dark:text-gray-400">Web Developer</p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <h3 className="text-xl font-semibold">Smiloops Inc</h3>
+          <p className="text-gray-500 dark:text-gray-400">2019 - 2021</p>
+          <p className="text-gray-500 dark:text-gray-400">Mobile Developer(Internship)</p>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+const Contact = (email: string) => {
+  return (
+    <React.Fragment>
+      <div className="flex flex-col space-y-2">
+        <p className="text-gray-500 dark:text-gray-400">{email}</p>
+        <div className="flex space-x-4">
+          <TwitterIcon />
+          <GithubIcon />
+          <LinkedinIcon />
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+type SectionElement = {
+  id: string;
+  content: React.JSX.Element;
+}
+
+const SectionElements: SectionElement[] = [
+  {
+    id: "Author",
+    content: Author(URL_LINKEDIN, URL_WANTEDLY)
+  },
+  {
+    id: "Work Experience",
+    content: WorkExperience()
+  },
+  {
+    id: "Contact",
+    content: Contact(MAIL)
+  },
+]
+
 export default function Home() {
   return (
-    <main className="flex-1 px-4 md:px-6 shadow-md">
-      <section className="py-5 md:py-10 lg:py-15">
-        <div className="container px-4 md:px-6">
-          <div className="space-y-3 text-center">
-            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">{AUTHOR}</h2>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-              Web(Full-stack) Developer, Tennis and Snowboard Lover. 🧑‍💻🎾🏂
-            </p>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-              I have experience in a
-              variety of technologies and I&apos;m always looking for new challenges to improve User Interface and Developer Experience.
-              <br />
-              <br />
-              Please see these links for more information.
-            </p>
-            <a href={URL_LINKEDIN} target="_blank" rel="noopener noreferrer" className="text-blue-700 hover:text-blue-900">LinkedIn</a>
-            , <a href={URL_WANTEDLY} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">Wantedly</a>
-          </div>
-        </div>
-      </section>
-
-      <section className="py-5 md:py-10 lg:py-15">
-        <div className="container px-4 md:px-6">
-          <div className="space-y-3 text-center">
-            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">Work Experience</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              <div className="flex flex-col space-y-2">
-                <h3 className="text-xl font-semibold">Commmune Inc</h3>
-                <p className="text-gray-500 dark:text-gray-400">2023 - Present</p>
-                <p className="text-gray-500 dark:text-gray-400">Web Developer/Team Leader</p>
-              </div>
-              <div className="flex flex-col space-y-2">
-                <h3 className="text-xl font-semibold">MonotaRO Inc</h3>
-                <p className="text-gray-500 dark:text-gray-400">2021 - 2023</p>
-                <p className="text-gray-500 dark:text-gray-400">Web Developer</p>
-              </div>
-              <div className="flex flex-col space-y-2">
-                <h3 className="text-xl font-semibold">Smiloops Inc</h3>
-                <p className="text-gray-500 dark:text-gray-400">2019 - 2021</p>
-                <p className="text-gray-500 dark:text-gray-400">Mobile Developer(Internship)</p>
-              </div>
+    <React.Fragment>
+      {SectionElements.map((element, _index) => (
+        <section key={element.id} className="py-5 md:py-10 lg:py-15">
+          <div className="container px-4 md:px-6">
+            <div className="space-y-3 text-center">
+              <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">{element.id}</h2>
+              {element.content}
             </div>
           </div>
-        </div>
-      </section>
-
-      <section className="py-5 md:py-10 lg:py-15">
-        <div className="container px-4 md:px-6">
-          <div className="space-y-3 text-center">
-            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl">Contact</h2>
-            <div className="flex flex-col space-y-2">
-              <p className="text-gray-500 dark:text-gray-400">{MAIL}</p>
-              <div className="flex space-x-4">
-                <TwitterIcon />
-                <GithubIcon />
-                <LinkedinIcon />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      ))}
+    </React.Fragment>
   )
 }
