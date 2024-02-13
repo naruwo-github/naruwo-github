6a6aedb7c42ec65750250bc024882026b7ef81b4
Narumi Nogawa <narutyan0711@gmail.com>
2024-02-13 15:26:30 +0900

    🔀 refactor(WorkExperience.tsx): replace hardcoded work experience data with dynamic data fetched from CMS 🎉 feat(cmsClient.ts): add sorting to work experience data query to display most recent experience first

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
+import { getWorkExperienceData } from '@/lib/cmsClient'
 
-export const WorkExperience: React.FC<{}> = () => {
+export const WorkExperience = async (): Promise<React.JSX.Element> => {
+    const data = await getWorkExperienceData()
+
+    if (data === null) return <p>Failed to fetch work experience data</p>
+
     return (
         <React.Fragment>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                <div className="flex flex-col space-y-2">
-                    <h3 className="text-xl font-semibold">Commmune Inc</h3>
-                    <p className="text-gray-500 dark:text-gray-400">2023 - Present</p>
-                    <p className="text-gray-500 dark:text-gray-400">Web Developer/Team Leader</p>
-                </div>
-                <div className="flex flex-col space-y-2">
-                    <h3 className="text-xl font-semibold">MonotaRO Inc</h3>
-                    <p className="text-gray-500 dark:text-gray-400">2021 - 2023</p>
-                    <p className="text-gray-500 dark:text-gray-400">Web Developer</p>
-                </div>
-                <div className="flex flex-col space-y-2">
-                    <h3 className="text-xl font-semibold">Smiloops Inc</h3>
-                    <p className="text-gray-500 dark:text-gray-400">2019 - 2021</p>
-                    <p className="text-gray-500 dark:text-gray-400">Mobile Developer(Internship)</p>
-                </div>
+                {data.results.map((result) => {
+                    // @ts-ignore
+                    const properties = result.properties
+                    const section = properties.section.title[0].plain_text
+                    const startedAt = properties.startedAt.date.start
+                    const endedAt = properties.endedAt.date?.start ? properties.endedAt.date.start : 'Present'
+                    const role = properties.role['rich_text'][0].plain_text
+                    return (
+                        <div key={section} className="flex flex-col space-y-2">
+                            <h3 className="text-xl font-semibold">{section} Inc</h3>
+                            <p className="text-gray-500 dark:text-gray-400">{startedAt} - {endedAt}</p>
+                            <p className="text-gray-500 dark:text-gray-400">{role}</p>
+                        </div>
+                    )
+                })}
             </div>
         </React.Fragment>
     )
