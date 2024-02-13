96971f33a1e1e1d344736a840381ea4302aa3fbb
Narumi Nogawa <narutyan0711@gmail.com>
2024-02-14 08:26:15 +0900

    refactor

--- src/app/api/work-experience/route.ts
+++ src/app/api/work-experience/route.ts
@@ -3,17 +3,28 @@ import { notion, workExperiencePageId } from '@/lib/cmsClient'
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const title = searchParams.get('title')
-  if (!title) return Response.redirect('/')
+  if (!title)
+    return new Response('Title parameter is required', { status: 400 })
 
-  const res = await notion.databases.query({
-    database_id: workExperiencePageId!,
-    filter: {
-      property: 'section',
-      rich_text: {
-        equals: title
+  try {
+    const res = await notion.databases.query({
+      database_id: workExperiencePageId!,
+      filter: {
+        property: 'section',
+        rich_text: {
+          equals: title
+        }
       }
+    })
+    if (res.results.length > 0) {
+      const properties = await res.results[0].properties
+      return Response.json(properties)
+    } else {
+      return new Response('No matching title found', { status: 404 })
     }
-  })
-  const properties = await res.results[0].properties
-  return Response.json(properties)
+  } catch (error) {
+    // TODO: Introduce a mechanism to collect logs
+    console.error(error)
+    return new Response('Internal Server Error', { status: 500 })
+  }
 }
